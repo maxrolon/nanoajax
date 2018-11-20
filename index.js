6f15875a04d41689427074ac55b8cc63d85b3f4e
Max Rolon <max.rolon@barrelny.com>
2018-11-20 16:58:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -72,8 +72,6 @@ exports.ajax = function (params, callback) {
   req.onabort = cb(null, 'Abort')
 
   if (body) {
-    setDefault(headers, 'XMLHttpRequest')
-
     if (!global.FormData || !(body instanceof global.FormData)) {
       setDefault(headers, 'Content-Type', 'application/x-www-form-urlencoded')
     }
